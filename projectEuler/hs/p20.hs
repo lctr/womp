32ed2cbf7fa6a453ffc1bac3ba7dfdd1e5b2168a
Lictor Guzman <lctrgzmn@gmail.com>
2021-01-31 12:43:29 -0600

    using digitToInt from Data.Char

---
+++ projectEuler/hs/p20.hs
@@ -0,0 +1,17 @@
+{-
+  PROJECT EULER -- PROBLEM 2-: FACTORIAL DIGIT SUM
+  @ https://projecteuler.net/problem=8
+-}
+
+import Data.Char ( digitToInt )
+
+factorialDigitSum :: (Show a, Num a, Enum a) => a -> Int
+factorialDigitSum n = sum . factorialDigits $ n
+  where 
+    factorial n = foldl (*) 1 [1..n]
+    digits s = map digitToInt . show $ s
+    factorialDigits n = digits . factorial $ n
+
+main = do 
+  putStrLn "the sum of the digits of 100! is"
+  return . factorialDigitSum $ 100
